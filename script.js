854878f91452561c6bf006caf62af65d859a14d5
Dan Tulen <dan20.tulen@gmail.com>
2024-11-28 15:47:43 +0100

    edity 11/28/24

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function setScreen(screen_id){
-    document.querySelectorAll(".screen").forEach(x => {x.style.display = x.id != screen_id ? "none" :"block"});
+    document.querySelectorAll(".screen").forEach(x => {x.style.display = x.id != screen_id ? "none" :"block";document.getElementById("ui").style.display = (x.id == "mainMenu" || x.id == "kal") ? "none" :"block"});
+}
+
+function kalkulaczka(){
+    setScreen("kal");
+}
+
+let t = 0;
+let m = 0;
+function poczitej(){
+    t = t + Number(document.getElementById("mass").value);
+    m = m + Number(document.getElementById("length").value);
+    document.getElementById("hmotnost").innerText = t;
+    document.getElementById("delka").innerText = m;
+    document.getElementById("mass").value = "";
+    document.getElementById("length").value = "";
+}
+function nuluj(){
+    t = 0;
+    m = 0;
+    document.getElementById("hmotnost").innerText = t;
+    document.getElementById("delka").innerText = m;
 }
