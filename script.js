e93df880307f6f56b179ab5e3f4dda32f5d29387
DanAKASurikata <dan20.tulen@gmail.com>
2025-01-23 15:44:01 +0100

    23/1/2025

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ let pgup = [3,-1,-1,4,3];
 let pgdn = [-1,-1,-1,0,-1];
 let pgrg = [2,0,4,-1,-1];
 let screen = 0;
+let penize = 5000;
 
 function setScreen(screen_id){
     document.querySelectorAll(".screen").forEach(x => {
         x.style.display = x.id != screen_id ? "none" :"block";
-        document.getElementById("ui").style.display = (x.id == "mainMenu") ? "none" :"block";
     });
+    document.getElementById("ui").style.display = (screen_id == "mainMenu") ? "none" :"block";
     screen = screen_id.replace("r", "");
     document.getElementById("lt").style.display = (pglt[screen] == -1) ? "none" :"block";
     document.getElementById("up").style.display = (pgup[screen] == -1) ? "none" :"block";
     document.getElementById("dn").style.display = (pgdn[screen] == -1) ? "none" :"block";
     document.getElementById("rg").style.display = (pgrg[screen] == -1) ? "none" :"block";
+    document.getElementById("zustatek").innerText = penize;
 }
 
 function pgLt(){
     setScreen(`r${pglt[screen]}`);
+    tutorial();
 }
 function pgUp(){
     setScreen(`r${pgup[screen]}`);
@@ -40,13 +43,41 @@ function startujeme(){
 function tutorial(){
     if(tutyes == true){
         if(tutprog == 0){
-            if(confirm("Přeješ si projít tutoriál?") == true){
+            if(confirm("Přeješ si projít tutoriál? \nVelmi ti to doporučuji =)") == true){
                 alert("Jdi do skladu a objednej vodu, vodku a jablečný mošt. \n\nZbytek zásob které potřebuješ ti přinesla babička.")
+                console.log("tutyes");
             } else {
                 tutyes = false;
+                console.log("tutno");
             }
+        }
+        if(tutprog == 1){
+            alert("Měl by jsi vidět možnosti nákupu. Kup požadované věci.")
         }
         tutprog++;
     }
-    console.log(tutyes);
+}
+
+let invent = [0,0,0,5,20];
+let invName = ["voda","vodka","moszt","babovka","bamburky"];
+function nakup(zbozi){
+    if(zbozi == 0){
+        invent[zbozi]++;
+        penize = penize - 50;
+    }
+    if(zbozi == 1){
+        invent[zbozi]++;
+        penize = penize - 450;
+    }
+    if(zbozi == 2){
+        invent[zbozi]++;
+        penize = penize - 150;
+    }
+    document.getElementById("zustatek").innerText = penize;
+    document.getElementById(invName[zbozi]).innerText = invent[zbozi];
+    if(penize < 1){
+        alert("Jsi šptaný, nemáš prachy =)");
+        setScreen("mainMenu");
+        alert("Pro novou hru dej znovu načíst stránku =)")
+    }
 }
